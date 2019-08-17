22d98e00ee98084be776a304ca40f1741aff1f87
Tony Fernandes <imbitubadigital@gmail.com>
2019-08-17 10:55:19 -0300

    Nodemon e Sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "hello word" });
+  return res.json({ message: "hello word angel" });
 });
 
-module.exports = routes;
+export default routes;
